54c7afbe73d635363a650b7ad41619478c3f5637
Bryan Ho <hutizaki@gmail.com>
2026-01-18 23:37:23 -0600

    Figuring out matrix multiplication on CUDA

---
+++ CUDA-Based-MNIST/Phase-3-CUDA-Fundamentals/CUDA-MNIST.cu
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <chrono>
+#include <vector>
+#include <cmath>
+#include <algorithm>
+#include <cuda_runtime.h>
+#include <device_launch_parameters.h>
+#include "../bryan-library/matrixMath.h"
